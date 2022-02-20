1329d008bd9fab2f33d8301734ed2bdaab29380a
burakdemirelli <bdemirelli23@my.sevkoleji.k12.tr>
2022-02-20 13:13:25 +0300

    added compressor

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,6 +1,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
@@ -25,6 +26,8 @@ public class RobotContainer {
   Climb climb = new Climb();
   Intake intake = new Intake();
 
+  Compressor compressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
+
 
   //Joysticks
   XboxController driver = new XboxController(0);
@@ -37,6 +40,7 @@ public class RobotContainer {
 
   public RobotContainer() {
     configureButtonBindings();
+    compressor.enableDigital();
   }
 
 
