ef5abc2df6911f908ef23adf7b9741c28483d488
burakdemirelli <bdemirelli23@my.sevkoleji.k12.tr>
2022-03-03 10:07:44 +0300

    changed wheel speeds

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -30,11 +30,11 @@ public class Intake extends SubsystemBase {
   }
 
   public void runForward(){
-    intakeMotor.set(ControlMode.PercentOutput, 0.75);
+    intakeMotor.set(ControlMode.PercentOutput, 0.88);
   }
 
   public void runBackwards(){
-    intakeMotor.set(ControlMode.PercentOutput, -0.75);
+    intakeMotor.set(ControlMode.PercentOutput, -0.88);
   }
 
   public void stop(){
